e3b985167cfd5ade83cac3dd17c9d188849557f0
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-02-23 15:23:22 -0500

    start with react-routing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-import SearchForm from './SearchForm'
-import SuperHeroListContainer from './SuperHeroListContainer'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Site from "./Site";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      superHero: "",
-      orderById: false
-    };
-  }
-
-  onSearch= (query, orderById) => {
-    this.setState({
-      superHero: query,
-      orderById: orderById
-    });
-  }
-
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <SearchForm onSearch={this.onSearch} />
-            </div>
-            <div className="col-6">
-              <SuperHeroListContainer  superHero={this.state.superHero} orderById={this.state.orderById} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Route path="/" component={Site} />
+      </Router>
     );
   }
 }
 
-export default App;
+export default App
